87e1fd67e096d3a4d71dd3fdaa19d59336b54579
Kunal Sunil Varudkar <varudkar.kunal@gmail.com>
2020-08-31 17:01:54 +0530

    Ip Change - ad-server

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,10 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://192.168.56.101:9000',
+       serverUrl: 'http://192.168.130.137:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
+
+// Appserver ip address - 192.168.130.137
